7a8ede3c87d5c82e79aff045281ee3ae94d0ce00
thoriqkhoir <thoriqkhoir537@gmail.com>
2026-08-24 11:30:26 +0700

    update build

---
+++ public/build/assets/SectionL1A7-AvDjLiZe.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-pWnsd7kM.js";import{TableL1A7 as e}from"./TableL1A7-BTLAJRuA.js";import"./table-Bt3_SKu4.js";import"./utils-jAU0Cazi.js";function p({totalAcquisitionCost:t,totalAmountNow:o}){return r.jsx("div",{className:"space-y-4",children:r.jsx(e,{totalAcquisitionCost:t,totalAmountNow:o})})}export{p as SectionL1A7,p as default};
